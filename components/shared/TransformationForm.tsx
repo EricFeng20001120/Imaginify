e776b0659f0d26e0043f579e2f27229d380dd3dd
EricFeng20001120 <fyx5620392@gmail.com>
2025-03-31 14:09:48 -0400

    image transformation with cloudItery, connect to database

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -24,10 +24,16 @@ import {
     SelectTrigger,
     SelectValue,
   } from "@/components/ui/select"
-import { useState, useTransition } from "react"
+import { useEffect, useState, useTransition } from "react"
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
 import { config } from "process"
 import { updateCredits } from "@/lib/actions/user.actions"
+import MediaUploader from "./MediaUploader"
+import TransformedImage from "./TransformedImage"
+import { getCldImageUrl } from "next-cloudinary"
+import { addImage, updateImage } from "@/lib/actions/image.actions"
+import { useRouter } from "next/navigation"
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal"
   
 
 export const formSchema = z.object({
@@ -54,6 +60,7 @@ const TransformationForm = ({action, data = null, userId, type, creditBalance, c
   const [isTransforming, setIsTransforming] = useState(false);
   const [transformationConfig, setTransformationConfig] = useState(config)
   const [isPending, startTransition] = useTransition()
+  const router = useRouter()
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
@@ -62,8 +69,70 @@ const TransformationForm = ({action, data = null, userId, type, creditBalance, c
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsSubmitting(true);
+    if (data || image){
+        const transformationUrl = getCldImageUrl({
+            width:image?.width,
+            height:image?.height,
+            src: image?.publicId,
+            ...transformationConfig
+        })
+
+        const imageData = {
+            title: values.title,
+            publicId: image?.publicId,
+            transformationType: type,
+            width: image?.width,
+            height: image?.height,
+            config: transformationConfig,
+            secureURL: image?.secureURL,
+            transformationURL: transformationUrl,
+            aspectRatio: values.aspectRatio,
+            prompt: values.prompt,
+            color: values.color,
+        }
+
+        if (action==='Add'){
+            try {
+                const newImage = await addImage({
+                    image: imageData,
+                    userId,
+                    path:'/'
+                })
+
+                if (newImage) {
+                    form.reset()
+                    setImage(data)
+                    router.push(`/transformations/${newImage._id}`)
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        if (action==='Update'){
+            try {
+                const updatedImage = await updateImage({
+                    image: {
+                        ...imageData,
+                        _id: data._id
+                    },
+                    userId,
+                    path:`/transformations/${data._id}`
+                })
+
+                if (updatedImage) {
+                    
+                    router.push(`/transformations/${updatedImage._id}`)
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+    setIsSubmitting(false)
+
     console.log(values)
   }
 
@@ -107,14 +176,22 @@ const TransformationForm = ({action, data = null, userId, type, creditBalance, c
     )
 
     setNewTransformation(null)
+
     startTransition(async () => {
-        // await updateCredits(userId, creditFee)
+        await updateCredits(userId, creditFee)
     })
   }
+
+  useEffect(() => {
+    if(image && (type==='restore' || type === 'removeBackground')) {
+        setNewTransformation(transformationType.config)
+    }
+  }, [image, transformationType.config, type])
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal/>}
         <CustomField
             control={form.control}
             name = "title"
@@ -201,6 +278,32 @@ const TransformationForm = ({action, data = null, userId, type, creditBalance, c
                 )}
             </div>
         )}
+
+        <div className="media-uploader-field">
+            <CustomField
+                control={form.control}
+                name = "publicId"
+                className="flex size-full flex-col"
+                render = {({field})=>(
+                    <MediaUploader
+                        onValueChange = {field.onChange}
+                        setImage = {setImage}
+                        publicId = {field.value}
+                        image = {image}
+                        type = {type}
+                    />
+                )}
+            />
+
+            <TransformedImage
+                image={image}
+                type = {type}
+                title = {form.getValues().title}
+                isTransforming={isTransforming}
+                setIsTransforming={setIsTransforming}
+                transformationConfig={transformationConfig}
+            />
+        </div>
 
         <div className="flex flex-col gap-4">
             <Button 
